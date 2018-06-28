bb3fb51ccba9621ce1c9d0278674eb5092aa61c0
Ivan Zotov <ivanzotov@gmail.com>
2018-06-28 20:28:01 +0300

    Fix

--- fns/index.js
+++ fns/index.js
@@ -1,12 +1,13 @@
 module.exports = {
-  'формат_даты': require('date-fns/format'),
+  'формат_даты': date => require('date-fns/format')(date, 'DD.MM.YYYY'),
   'дней_между_датами': require('date-fns/difference_in_days'),
   'взять_число': require('date-fns/get_date'),
   'добавить_месяцев': require('date-fns/add_months'),
   'добавить_дней': require('date-fns/add_days'),
-  'даты_равны?': require('date-fns/is_equal'),
+  'даты_равны': require('date-fns/is_equal'),
   'дней_в_году': require('date-fns/get_days_in_year'),
   'округлить': require('./round'),
   'денежный_формат': require('./currencyFormat'),
   'расчет_ежемесячного_платежа': require('./annuity'),
+  'вывести': require('./log'),
 }
